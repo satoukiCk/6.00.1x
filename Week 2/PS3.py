45b67a9074d3b56334e42a60e3a24a5a98442551
cyw <weems1995@hotmail.com>
2016-06-12 23:19:22 +0800

    Revert "Revert "Revert "Revert "Revert "Week 2 Lecture 4 and Problem Set 1"""""
    This reverts commit f5726209ea3de7ba006fd1ac12af1a85e2dd58bc.

--- Week 2/PS3.py
+++
@@ -1,6 +0,0 @@
-def item_order(order):
-    ham = order.count("hamburger")
-    sal = order.count("salad")
-    wat = order.count("water")
-    result = "salad:" + str(sal) + " hamburger:" + str(ham) + " water:" + str(wat);
-    return result;
