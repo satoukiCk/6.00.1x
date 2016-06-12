a9da378d26c3ff29e16336fba32f89354d06e5ac
cyw <weems1995@hotmail.com>
2016-06-12 15:37:36 +0800

    Week 2
    Lecture 3 completed!

---
+++ Week 1/1.py
@@ -0,0 +1,9 @@
+if (isinstance(varA,str) or isinstance(varB,str)):
+    print("string involved")
+else:
+    if (varA > varB):
+        print("bigger")
+    elif (varA == varB):
+        print("equal")
+    else:
+        print("small")
---
+++ Week 2/L3 5.py
@@ -0,0 +1,8 @@
+print "Hello!"
+for i in range(10,1,-2):
+    print i
+
+n=0
+for i in range(end+1):
+    n+=i
+print n
