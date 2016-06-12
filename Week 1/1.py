f938b60d56f40de610ddc68ed15a50b7d711a6cd
cyw <weems1995@hotmail.com>
2016-06-12 15:32:53 +0800

    Revert "Signed-off-by: cyw <weems1995@hotmail.com>"
    This reverts commit 7b0222dc3fe9b1dcb51f69ace76f314d5d45e983.

--- Week 1/1.py
+++
@@ -1,9 +0,0 @@
-if (isinstance(varA,str) or isinstance(varB,str)):
-    print("string involved")
-else:
-    if (varA > varB):
-        print("bigger")
-    elif (varA == varB):
-        print("equal")
-    else:
-        print("small")
--- Week 2/L3 5.py
+++
@@ -1,8 +0,0 @@
-print "Hello!"
-for i in range(10,1,-2):
-    print i
-
-n=0
-for i in range(end+1):
-    n+=i
-print n
